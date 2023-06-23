3ea7d57b47d366e343052f2ed568b14da282c9ca
wilfredcloud <wilfredcloudspace@gmail.com>
2023-06-23 23:17:00 +0100

    feat: Add flask version

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,7 +5,7 @@ package { 'python3-pip':
 }
 
 exec { 'install-flask':
-  command => 'pip3 install flask',
+  command => 'pip3 install flask==2.1.0',
   path    => ['/usr/bin', '/usr/local/bin'],
   require => Package['python3-pip'],
 }
